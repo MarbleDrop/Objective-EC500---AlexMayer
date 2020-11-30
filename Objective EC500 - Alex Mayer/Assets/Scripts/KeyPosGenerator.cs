6c0af4fa923ddb2c5affa93b6070af6e98716352
ALBERTOMK01\alexm <alexander.mayer@lethbridgecollege.ca>
2020-11-29 20:02:51 -0700

    Added all objects (Exept "Bookcase" and "Plant Pot") All objects are textured and have prefabs. Keys have been placed near spawn positions. Edited Glass Materials. Added Reflection Probe.

---
+++ Objective EC500 - Alex Mayer/Assets/Scripts/KeyPosGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPosGenerator : MonoBehaviour
+{
+    public GameObject Key1;
+    public GameObject key2;
+    public GameObject key3;
+    public GameObject key4;
+    public GameObject key5;
+
+    public int randomNum;
+
+    void Start()
+    {
+        randomNum = Random.Range(1, 6);
+        if (randomNum == 1)
+        {
+            Key1.SetActive(true);
+        }
+        else if(randomNum == 2)
+        {
+            key2.SetActive(true);
+        }
+        else if(randomNum == 3)
+        {
+            key3.SetActive(true);
+        }
+        else if(randomNum == 4)
+        {
+            key4.SetActive(true);
+        }
+        else
+        {
+            key5.SetActive(true);
+        }
+    }
+
+    
+}
