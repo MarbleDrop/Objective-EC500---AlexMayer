77651978e6bf5ebf5c3e90729c8bce811faabd72
ALBERTOMK01\alexm <alexander.mayer@lethbridgecollege.ca>
2020-12-13 19:38:49 -0700

    Added WalterTheFish (Compleatly animated and textured) and added script to him to make him look at the player.

---
+++ Objective EC500 - Alex Mayer/Assets/Follow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject Target;
+
+    void Update()
+    {
+        // Makes the fish look at the player while only rotating on the y axis.
+
+        Vector3 targetPosition = new Vector3(Target.transform.position.x, transform.position.y, Target.transform.position.z);
+        transform.LookAt(targetPosition);
+    }
+}
